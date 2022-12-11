30ef4cf6db88d430cd62d24eb2bcef8f4c962716
Pradeep <Pradeep@SEZLAP-9804.india.rsystems.com>
2022-12-11 14:33:08 +0530

    update mail

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -12,7 +12,7 @@ interface IMail {
     to: string;
     subject: string;
     text: string;
-    html: string;
+    html?: string;
 }
 
 interface mailerBody {
@@ -27,38 +27,47 @@ export class Mailer {
     private _transport: nodemailer.Transporter;
     constructor() {
         this._transport = nodemailer.createTransport(
-            sgGrid({
-                apiKey: config.EMAIL_API_KEY,
-            })
+            // sgGrid({
+            //     apiKey: config.EMAIL_API_KEY,
+            // })
+            {
+                host: "smtp.mailtrap.io",
+                port: 2525,
+                auth: {
+                    user: "a6128f2fd18cd1",
+                    pass: "a4440f745f5222",
+                },
+            }
         );
     }
 
     static container(body: mailerBody) {
         const { local, email, token, user, templateName } = body;
         const mailer = new Mailer();
+
         const readTemplate = fs.readFileSync(
             path.join(__dirname, "..", `views/${templateName}.ejs`),
             "utf-8"
         );
         const template = ejs.compile(readTemplate);
-        if (user.role !== "admin") {
-            mailer.send({
-                to: email,
-                from: config.MAILER_ID,
-                subject: "Registration successfull",
-                text: `Welcome to ${user.firstname} ${user.lastname}`,
-                html: template({
-                    email: email,
-                    firstname: user.firstname,
-                    lastname: user.lastname,
-                    token: token,
-                    localURL: local,
-                }),
-            });
-        }
+
+        mailer.send({
+            to: email,
+            from: config.MAILER_ID,
+            subject: "Registration successfull",
+            text: `Welcome to ${user.firstname} ${user.lastname}`,
+            // html: template({
+            //     email: email,
+            //     firstname: user.firstname,
+            //     lastname: user.lastname,
+            //     token: token,
+            //     localURL: local,
+            // }),
+        });
     }
 
     send(option: IMail) {
+        console.log("option", option);
         this._transport.sendMail(option, (error) => {
             console.log(error);
             if (error) throw new BadRequestError(error.message);
