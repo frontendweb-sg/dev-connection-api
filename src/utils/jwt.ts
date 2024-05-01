a65e255695fd8a3b1ce4534d0700f68f102712fc
frontendweb-sg <frontendweb.sg@gmail.com>
2024-05-01 15:40:41 +0530

    update jwt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,13 +4,14 @@ import { AuthError } from "../errors";
 interface JwtOption extends SignOptions {}
 
 const DEFAULT_OPTIONS: JwtOption = {
-  expiresIn: "1H",
+  expiresIn: "1h",
 };
 
 export class Jwt {
   static genToken(payload: JwtPayload, option: JwtOption = DEFAULT_OPTIONS) {
     return JWT.sign(payload, process.env.SECRET_KEY!, option);
   }
+
   static verifyToken(token: string) {
     return JWT.verify(token, process.env.SECRET_KEY!, (error, decoded) => {
       if (!error) return decoded;
